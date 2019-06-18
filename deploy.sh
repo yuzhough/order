bd57606715ae9bb603aa7ffecfc87bcb20b4763c
yuzhough <yuzhou643@163.com>
2019-06-18 20:39:36 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 #需要配置如下参数
 # 项目路径, 在Execute Shell中配置项目路径, pwd 就可以获得该项目路径
 # export PROJ_PATH=这个jenkins任务在部署机器上的路径
-export PROJ_PATH=/root/project/order
+export PROJ_PATH=/root/project
 
 # 输入你的环境上tomcat的全路径
 # export TOMCAT_APP_PATH=tomcat在部署机器上的路径
